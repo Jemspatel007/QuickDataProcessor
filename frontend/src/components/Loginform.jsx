b061fc7aff1ca64469bde056274c02eb81fe5d25
SIVAPRAKASH CHITTU HARIHARAN <sv470985@dal.ca>
2024-11-27 00:38:44 -0400

    Close Modal after success

--- frontend/src/components/Loginform.jsx
+++ frontend/src/components/Loginform.jsx
@@ -41,15 +41,12 @@ const LoginForm = () => {
     if (currentStep === 1) {
       const apiUrl = `${baseUrl}/dev/auth/getSecurityQuestions`;
       const formData = { email, password };
-      console.log("Formdata", formData);
       try {
         const response = await axios.post(apiUrl, formData, {
           headers: {
             "Content-Type": "application/json",
           },
         });
-        console.log("res", response);
-
         const parsedBody = response.data.body;
         console.log("pas", parsedBody);
         if (response.data.statusCode === 200) {
@@ -80,6 +77,8 @@ const LoginForm = () => {
           if (token) {
             setUserLoggedIn(token);
             // localStorage.setItem("token", token);
+            console.log("dsfsdf",setUserLoggedIn)
+            console.log("token",token)
             toast.success("Login successful!");
             navigate("/home"); 
           } else {
